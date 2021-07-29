299f30682e190f5df9b69b4fde95a0c35aa1b8a3
qs <1477784201@qq.com>
2021-07-29 23:27:33 +0800

    [add]二叉树的前序后续中序层序遍历

--- src/main/java/leetcode/binaryTree/Traversal.java
+++ src/main/java/leetcode/binaryTree/Traversal.java
@@ -1,8 +1,6 @@
 package leetcode.binaryTree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Traversal {
 
@@ -41,5 +39,147 @@ public class Traversal {
             if(pop.left!=null) stack.push(pop.left);
         }
         return list;
+    }
+
+    private List<Integer> preorderTraversal2(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root!=null || !stack.empty()){
+            while (root!=null) {
+                list.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop().right;
+        }
+        return list;
+    }
+
+
+    /**
+     * 中序遍历的递归写法
+     * @param root 根节点
+     * @return 遍历的列表
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root,list);
+        return list;
+    }
+
+    /**
+     * 中序遍历递归
+     * @param root 根节点
+     * @param list 列表
+     */
+    private void inorderTraversal(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        inorderTraversal(root.left,list);
+        list.add(root.val);
+        inorderTraversal(root.right,list);
+    }
+
+    private List<Integer> inorderTraversal1(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while(node!=null||!stack.isEmpty()){
+            if(node !=null){
+                stack.push(node);
+                node = node.left;
+            }else{
+                node = stack.pop();
+                list.add(node.val);
+                node = node.right;
+
+            }
+        }
+        return list;
+    }
+
+    private List<Integer> inorderTraversal2(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        while(root!=null||!stack.isEmpty()){
+            while (root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+        return list;
+    }
+
+    /**
+     * 后续遍历
+     * @param root 根节点
+     * @return 列表
+     */
+    private List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversal(root,list);
+        return list;
+    }
+
+    private void postorderTraversal(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        postorderTraversal(root.left,list);
+        postorderTraversal(root.right,list);
+        list.add(root.val);
+
+    }
+
+    private List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root!=null||!stack.empty()){
+            while (root!=null){
+                list.add(root.val);
+                stack.push(root);
+                root = root.right;
+            }
+            root = stack.pop().left;
+        }
+        Collections.reverse(list);
+        return list;
+
+    }
+
+    private List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if(root==null) return lists;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int size;
+        TreeNode cur;
+        while (!queue.isEmpty()){
+            size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                cur = queue.poll();
+                list.add(cur.val);
+                if(cur.left!=null) queue.add(cur.left);
+                if(cur.right!=null) queue.add(cur.right);
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(4);
+        treeNode.right = new TreeNode(1);
+        treeNode.left.left = new TreeNode(2);
+        treeNode.left.right = new TreeNode(3);
+        treeNode.right.right = new TreeNode(5);
+        new Traversal().levelOrder(treeNode).forEach(System.out::println);
+
     }
 }
