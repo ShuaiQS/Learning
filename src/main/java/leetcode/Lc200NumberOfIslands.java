fde4fbd70f0460d2b4cb0b8412bfcf854c3d3bd6
qs <1477784201@qq.com>
2021-10-24 15:30:19 +0800

    [add]leetcode200题岛屿数量

--- src/main/java/leetcode/Lc200NumberOfIslands.java
+++ src/main/java/leetcode/Lc200NumberOfIslands.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Lc200NumberOfIslands {
     private int m;
     private int n;
@@ -18,6 +21,13 @@ public class Lc200NumberOfIslands {
         return result;
 
     }
+
+    /**
+     * 深度优先算法
+     * @param grid 二维网格
+     * @param i x
+     * @param j y
+     */
     private void dfs(char[][] grid,int i, int j){
         if(i<0||i>=m||j<0||j>=n) return;
         if(grid[i][j]=='1'){
@@ -26,6 +36,27 @@ public class Lc200NumberOfIslands {
             dfs(grid,i+1,j);
             dfs(grid,i,j-1);
             dfs(grid,i,j+1);
+        }
+    }
+
+    /**
+     * 广度优先算法
+     * @param grid 二维网格
+     * @param i x
+     * @param j y
+     */
+    private void bfs(char[][] grid,int i, int j){
+        Queue<Integer> queue = new LinkedList<>();
+        grid[i][j]='0';
+        queue.add(i*n+j);
+        while (!queue.isEmpty()){
+            Integer poll = queue.poll();
+            int x = poll/n;
+            int y = poll%n;
+            if(x-1>=0&&grid[x-1][y]=='1') {grid[x-1][y]='0';queue.add((x-1)*n+y);}
+            if(y-1>=0&&grid[x][y-1]=='1') {grid[x][y-1]='0';queue.add((x)*n+y-1);}
+            if(x+1<m&&grid[x+1][y]=='1') {grid[x+1][y]='0';queue.add((x+1)*n+y);}
+            if(y+1<n&&grid[x][y+1]=='1') {grid[x][y+1]='0';queue.add((x)*n+y+1);}
         }
     }
 }
